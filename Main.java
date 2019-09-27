e1d75954733049ea60164975940edf0b01534c1e
Carlos Pambo <kidinho17@gmail.com>
2019-09-27 09:53:49 +0200

    add read me

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.println("Hello World!");
-
         for (int i = 1; i <= 10; i++) {
-            System.out.println(String.format("Number %s ", i));
+            System.out.println(String.format("Number %s ", i * 2));
         }
     }
 }
